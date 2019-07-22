27f1ff5ff7edc5e36ad1cf70a448fb810b6fe3bc
tigarto <helpu1409@gmail.com>
2019-07-22 14:15:49 -0500

    Update example3.c

--- codigo/example3.c
+++ codigo/example3.c
@@ -18,7 +18,7 @@ int main (void){
   int estado;
   printf("Empezando proceso %d\n", getpid());  
   if(fork() == 0) {
-     // Codigo hijo
+     // Código hijo
      rol = 'H';
      printf("Empezando proceso %d\n", getpid()); 
      fin = fin + 3;
@@ -28,26 +28,25 @@ int main (void){
      }
    }
    else {
-     // Codigo padre
+     // Código padre
      rol = 'P';
      for(i = 0; i < fin; i++) {
        sleep(1);
        printf("%c -> i = %d\n",rol ,i);
      }
+     int pid_h_espera = wait(&estado);
+     if (pid_h_espera == 0) {
+       printf("Bien chino - status: %d\n",estado);
+       printf("Finish him: %d\n", pid_h_espera);
+     }
+     else {
+       printf("Esperando al chino - status: %d\n",estado);
+       printf("pid espera: %d\n", pid_h_espera);
+     }   
+     printf("\n");
    }
-   // Codijo ejecutado por ambos (para el caso)
+   // Código ejecutado por ambos (para el caso)
    printf("%c -> i = %d\n",rol ,i);
-   // Espera del padre
-   int pid_h_espera = wait(&estado);
-   if (pid_h_espera == 0) {
-     printf("Bien chino - status: %d\n",estado);
-     printf("Finish him: %d\n", pid_h_espera);
-   }
-   else {
-     printf("Esperando al chino - status: %d\n",estado);
-     printf("pid espera: %d\n", pid_h_espera);
-   }   
-   printf("\n");
    printf("Finalizando proceso %d\n", getpid());
    return 0;
 }
